487a239e2afd45b93822e4604a2a913062d3bf9e
DorianLeona <MsDorianBell@outlook.com>
2022-12-26 20:37:16 -0500

    Started working on Ingredient/Product stuff

--- Ingredient.java
+++ Ingredient.java
@@ -1,3 +1,30 @@
 import java.util.*;
 
- 
+public class Ingredient
+{
+	String name;
+	double amount;
+	double amount_saleUnits;
+	double salePrince;
+	String units;
+	String saleUnits;
+
+
+
+	public Ingredient()
+	{
+		this.name = "blank";
+		this.amount = 0.00;
+		this.amount_saleUnits = 0.00;
+		this.salePrice = 0;
+		this.units = "NULL";
+		this.saleUnits = "NULL";
+	};
+
+
+	public void init(String dataString)
+	{
+
+	};
+}
+//End class//
---
+++ Pie.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Pie extends Product
+{
+	Component garnish;
+	Component filling;
+	Component crust;
+
+	public Pie()
+	{
+		this.name = "blank";
+		this.type = "generic";
+		this.amount = new int[50];
+		this.ingredients = new ArrayList<Ingredient>();
+		this.requiredIngredients = new ArrayList<String>();
+	};
+
+
+	public void init(String dataString)
+	{
+		
+	};
+
+
+	public void displayIngredients()
+	{
+		int total_possible_ingredients = 50;
+		
+		System.out.println("\n\n========== Required Ingredients ==========\n\n");
+		for(int i = 0; i < total_possible_ingredients; i++)
+		{
+			if(this.amount != 0)
+			{
+				String name = this.ingredients.getName();
+				double amount = this.ingredients.getAmount();
+				String amount = this.getUnits();
+
+				System.out.printf("%s		%lf %s", name, amount, units);
+				System.out.println("\n");
+			}
+		}
+			System.out.println("\n");
+	};
+
+
+	public ArrayList<Ingredient> getIngredientList()
+	{
+		return this.ingredients;
+	};
+	
+
+	public String getIngredientNames()
+	{
+		return this.requiredIngredients;
+	};
+}
+//End class//
