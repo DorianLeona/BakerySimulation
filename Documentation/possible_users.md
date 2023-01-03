45ea09ce11269f79e9f7490368231398008ce66b
DorianLeona <MsDorianBell@outlook.com>
2023-01-02 20:19:24 -0500

    Added to menu. Completed Ingredient class. Next: Write the control files needed for this program.

---
+++ Documentation/possible_users.md
@@ -0,0 +1,32 @@
+#Customer
+
+*Abilities*
+	- Make an Order
+	- Update an Unprocessed Order
+	- Cancel an Unprocessed Order
+	- Request a Refund
+	- View All Pending Orders Under Their Account
+	- View Entire Order History
+
+
+#Employee
+*Who is This?*
+	- An employee is a person who works at the bakery and has recieved training satisfactory for the fundamental roles of operating the business and the bakery's order management program.
+*Abilities*
+	-
+
+
+#Manager
+*Who is This?*
+	- This is Bella herself. She is the owner of the establishment, and as such, she knows more about the bakery's program, the recipes, and the business than the regular employees. 
+
+
+#Technician
+*Who is This?*
+	- The technician is a contractor who has the proper training to handle any maintenance, testing, or other advanced utilities of the program used by the bakery.
+*Abilities*
+	- Run Customer Mode Tests
+	- Run Employee Mode Tests
+	- Run Manager Mode Tests
+	- Freeze / Unfreeze System
+
--- Ingredient.java
+++ Ingredient.java
@@ -1,36 +1,54 @@
 import java.util.*;
+import java.io.*;
 
 public class Ingredient
 {
 	String name;
-	double amount;
-	double amount_saleUnits;
-	double salePrince;
+	double pricePerUnit;
 	String units;
-	String saleUnits;
+	double amount;
 
 
-
+	//Null constructor//
 	public Ingredient()
 	{
 		this.name = "blank";
 		this.amount = 0.00;
-		this.amount_saleUnits = 0.00;
-		this.salePrice = 0;
+		this.pricePerUnit = 0.00;
 		this.units = "NULL";
-		this.saleUnits = "NULL";
 	};
 
 
+	//Set members to object-specific values//
 	public void init(String dataString)
 	{
+		String[] data = dataString.split("/");
 
+
+		this.name = data[0];
+			
+		String price_string = data[1];
+		this.pricePerUnit = Double.parseDouble(price_string);
+		
+
+		this.units = data[2];
+
+		String amount_string = data[3];
+		this.amount = Double.parseDouble(amount_string);
 	};
 
 
-	public void getAmount()
+	//Return required amount of this Ingredient//
+	public double getAmount()
 	{
 		return this.amount;
+	};
+
+
+	//Return the standard baking unit for this Ingredient//
+	public String getUnits()
+	{
+		return this.units;
 	};
 }
 //End class//
